e9eefacaa758dc79d75c030a7f054b75b7c296a9
Yakovlev <jakovlev_artem@mail.ru>
2019-02-12 15:44:35 +0300

    2st web view app

---
+++ src/main/java/ru/itpark/reportweb/ReportWebApplication.java
@@ -0,0 +1,14 @@
+package ru.itpark.reportweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportWebApplication.class, args);
+    }
+
+}
+
---
+++ src/main/resources/sql/data-h2.sql
@@ -0,0 +1,3 @@
+INSERT INTO incidents (id, name, content) VALUES
+  (1, 'First incident', 'First incident content'),
+  (2, 'Second incident', 'Second incident content');
